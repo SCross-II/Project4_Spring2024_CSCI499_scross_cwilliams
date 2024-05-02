c9f59c681cd00ea782684b175150ebd3f050a85b
cameronwill11 <122390444+cameronwill11@users.noreply.github.com>
2024-05-02 15:09:55 -0500

    Add files via upload

--- random.php
+++ random.php
@@ -4,7 +4,31 @@
     <title>Sports Quiz</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
+
 <body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Sports Quiz</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <!-- Link to Home Page -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="main.php">Home</a>
+                    </li>
+                    <!-- Link to Login/Signup Page -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="random.php">More Trivia</a>
+                    </li>
+                    <!-- Link to Logout Page -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     <div class="container">
         <h2>Sports Quiz</h2>
 
